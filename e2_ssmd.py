a9d5ad6f19853b6d9f0603204e3abf5e84f46f91
JvcCoding <joaovitorcordeiro2008@gmail.com>
2025-12-16 20:50:44 -0300

    Estudo sobre soma, subtracao, multiplacao e divisao

---
+++ e2_ssmd.py
@@ -0,0 +1,13 @@
+#a matematica funciona com os sinais basicos que imaginamos
+#soma = +
+soma = 2 + 3
+print("Soma: ", soma)
+#subtracao = -
+subtracao = 5 - 2
+print("Subtração: ", subtracao)
+#multiplicacao = *
+multiplicacao = 4 * 3
+print("Multiplicação: ", multiplicacao)
+#divisao = /
+divisao = 10 / 2
+print("Divisão: ", divisao)
