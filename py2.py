7c7b42d6dacdf658bd3c529324d0549cc01f314d
JvcCoding <joaovitorcordeiro2008@gmail.com>
2025-12-16 20:07:00 -0300

    alterations

---
+++ py2.py
@@ -0,0 +1,4 @@
+mes = 2.6
+dia = 0.2
+resultado = mes + dia
+print(resultado)
---
+++ py3.py
@@ -0,0 +1,3 @@
+numero1 = input("eescreva uma palavra")
+numero2 = input("escolha outra palavra")
+print(numero1 + numero2)
