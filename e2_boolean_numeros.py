cf514336a66d58475712718accf0619480c05c5d
JvcCoding <joaovitorcordeiro2008@gmail.com>
2025-12-16 20:57:34 -0300

    Estudando boolean e suas comparaçoes entre numeros

---
+++ e2_boolean_numeros.py
@@ -0,0 +1,17 @@
+#boolean se refere á True e False, onde True é 1 e False é 0
+#com um booleano podemos fazer comparações entre valores
+#maior que = >
+maior = 5 > 2
+print("Maior que: ", maior)
+#menor que = <
+menor = 2 < 5
+print("Menor que: ", menor)
+#igual a = ==
+igual = 3 == 3
+print("Igual a: ", igual)
+#diferente de = !=
+diferente = 4 != 2
+print("Diferente de: ", diferente)
+#maior ou igual a = >=
+maior_igual = 5 >= 5
+print("Maior ou igual a: ", maior_igual)
