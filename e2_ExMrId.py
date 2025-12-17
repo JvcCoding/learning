f3c107efaa61278b6f79636476fd29b0a71f5cba
JvcCoding <joaovitorcordeiro2008@gmail.com>
2025-12-16 23:54:25 -0300

    aqui estudei sobre exponentes, remainder e integer division

---
+++ e2_ExMrId.py
@@ -0,0 +1,12 @@
+#aqui estudarei sobre exponentes, remainder e integer division
+#exponentes são representados por dois asteriscos (**), onde o numero que vem apos é o numero que estará sendo elevado
+exponente = 2 ** 3
+print("Exponente: ", exponente)
+
+#remainder é representado pelo simbolo de porcentagem (%), onde o resultado é o resto de uma divisão
+remainder = 10 % 3
+print("Remainder: ", remainder)
+
+#integer division é representada por duas barras (//), onde o resultado é a parte inteira da divisão
+integer_division = 10 // 3
+print("Integer Division: ", integer_division)
