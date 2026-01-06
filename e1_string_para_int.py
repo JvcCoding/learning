d5af4fb932b269de3e6cd67d8c5833c63e666fc1
tevoshw <tevoshw@gmail.com>
2026-01-06 19:25:17 -0300

    Improve lucky number calculation and output
    Refactor input handling and output formatting for lucky number calculation.

--- e1_string_para_int.py
+++ e1_string_para_int.py
@@ -1,5 +1,6 @@
-dialogo = input("o seu numero da sorte é a soma do seu mes de nascimento com o dia do seu nascimento! ")
-mes = input("Digite o mês do seu nascimento (1-12): ")
-dia = input("Digite o dia do seu nascimento (1-31): ")
-numero_da_sorte = int(mes) + int(dia)
-print("Seu número da sorte é: ", numero_da_sorte)
+
+print("O seu numero da sorte é a soma do seu mes de nascimento com o dia do seu nascimento! ") # JUST DO A PRINT, NOT A INPUT
+mes = int(input("Digite o mês do seu nascimento (1-12): ")) # TRANSFORM TO INT IN THIS LINE
+dia = (input("Digite o dia do seu nascimento (1-31): ")) # TRANSFORM TO INT IN THIS LINE
+# numero_da_sorte = int(mes) + int(dia) DONT NEED TO DO THAT, JUST USE IN THE PRINT BELOW
+print(f"Seu número da sorte é: {mes + dia}") # USE A F-STRING
